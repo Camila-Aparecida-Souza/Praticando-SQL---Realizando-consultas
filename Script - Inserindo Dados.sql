12d0582014a34d0a89c1040578d58b8c62dab039
Camila-Aparecida-Souza <camilaaparecida593@gmail.com>
2026-04-04 16:19:12 -0300

    Remoção de espaços em branco da TabelaEmprestimo

--- Script - Inserindo Dados.sql
+++ Script - Inserindo Dados.sql
@@ -37,10 +37,10 @@ VALUES
 
 INSERT INTO TabelaEmprestimo (id_emprestimo, Status, Prazo, DataInicio, Tipo, Valor, id_cliente)
 VALUES
-('E01', 1, 12, '2023-01-01', '  Pessoal', 5000.00, 'CL01'),
-('E02', 0, 24, '2023-02-15', '  Imobiliário', 100000.00, 'CL02'),
-('E03', 1, 36, '2023-03-10', 'Automóvel  ', 25000.00, 'CL03'),
-('E04', 1, 18, '2023-04-20', ' Consignado', 15000.00, 'CL04'),
+('E01', 1, 12, '2023-01-01', 'Pessoal', 5000.00, 'CL01'),
+('E02', 0, 24, '2023-02-15', 'Imobiliário', 100000.00, 'CL02'),
+('E03', 1, 36, '2023-03-10', 'Automóvel', 25000.00, 'CL03'),
+('E04', 1, 18, '2023-04-20', 'Consignado', 15000.00, 'CL04'),
 ('E05', 0, 48, '2023-05-18', 'Pessoal', 7000.00, 'CL05');
 
 
